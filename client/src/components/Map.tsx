346dc860160c255fd7007ee58562f78b0a6e925e
dokratos <dokratos@hotmail.it>
2023-07-05 16:04:17 +0200

    add google maps

---
+++ client/src/components/Map.tsx
@@ -0,0 +1,56 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { useState, useEffect } from 'react';
+
+// const loader = new Loader({
+//   apiKey: "AIzaSyBjuCY26HswvhBf-odx9mlouC9ITEWC_gI",
+//   version: "weekly"
+// });
+
+// loader.load().then(async () => {
+//   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+//   map = new Map(document.getElementById("map") as HTMLElement, {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// });
+
+
+export const Map = () => {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyBjuCY26HswvhBf-odx9mlouC9ITEWC_gI"
+  });
+
+  const [map, setMap] = useState(null);
+  const [center, setCenter] = useState({
+    lat: 40.4168,
+    lng: -3.70379
+  });
+
+  useEffect(() => {
+    setCenter((center) => ({ lat: -25.344, lng: 131.031 }));
+  }, []);
+
+  // @ts-ignore
+  const handleOnLoad = (map) => {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  };
+  return (
+    <GoogleMap
+            onLoad={handleOnLoad}
+            center={center}
+            zoom={15}
+            mapContainerStyle={{ width: '100%', height: '100vh' }}
+            options={{
+              zoomControl: true,
+              streetViewControl: false,
+              mapTypeControl: true,
+              fullscreenControl: true,
+              minZoom: 11,
+              maxZoom: 15
+            }}
+          ></GoogleMap>
+  )
+}
+
