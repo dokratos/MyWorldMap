d6aee604abeb3e367ecc322b7746badbf0917e98
dokratos <dokratos@hotmail.it>
2023-07-05 14:00:58 +0200

    server

---
+++ server/src/main/java/server/myWorldMap/MyWorldMapApplication.java
@@ -0,0 +1,13 @@
+package server.myWorldMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorldMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorldMapApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/server/myWorldMap/MyWorldMapApplicationTests.java
@@ -0,0 +1,13 @@
+package server.myWorldMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorldMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
